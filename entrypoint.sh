10dd1e5431616593d90166441afec9eee97a2270
Konstantin Aksenov <aksenov.kostya@gmail.com>
2023-07-10 09:30:46 +1000

    minor fixes

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,21 +10,22 @@ fi
 status=0
 app=$1
 test=$2
-device-id=$3
-os-version-id=$4
+deviceid=$3
+osversionid=$4
 orientation=$5
 type=$6
 locale=$7
-num-flaky-test-attempts=$8
+numflakytestattempts=$8
 timeout=$9
 
+service_account_file=/opt/service_account.json
 echo "$SERVICE_ACCOUNT" > $service_account_file
 project_id=$(cat $service_account_file | jq -r ".project_id")
 
 gcloud auth activate-service-account --key-file=$service_account_file
 gcloud config set project $project_id
 
-if gcloud firebase test android run --app=$app --test=$test --type=$type --device=model=$device-id,version=$os-version-id,locale=$locale,orientation=$orientation --num-flaky-test-attempts=$num-flaky-test-attempts --timeout=$timeout
+if gcloud firebase test android run --app=$app --test=$test --type=$type --device=model=$deviceid,version=$osversionid,locale=$locale,orientation=$orientation --num-flaky-test-attempts=$numflakytestattempts --timeout=$timeout
 then
     echo "Test execution successfully finished"
 else
